88c0fd43a8ad2940d70fc41f5fb3b163575e3ada
Faca857-SD <faucastro857@gmail.com>
2025-08-10 17:02:48 -0600

    Se pone el codigo de la clase main por primera vez para ver que funcione todo

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -4,10 +4,31 @@
  */
 package app;
 
-/**
- *
- * @author Anguar Alberto Rodriguez Fonseca
- */
+import ui.LoginFrame;
+import infrastructura.DatabaseConnection;
+import servicio.LogService;
+import javax.swing.*;
+
 public class MainApp {
-    
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            LogService.getInstance().logError("Error al configurar Look and Feel", e);
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            try {
+                DatabaseConnection.getInstance().testConnection();
+                LogService.getInstance().logInfo("Aplicación iniciada correctamente");
+                new LoginFrame().setVisible(true);
+            } catch (Exception e) {
+                LogService.getInstance().logError("Error al iniciar aplicación", e);
+                JOptionPane.showMessageDialog(null, 
+                    "Error al conectar con la base de datos", 
+                    "Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
+        });
+    }
 }
